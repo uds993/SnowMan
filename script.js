5014a908d8089570b81b0130817940ec5a8d2712
Uday Solanki <uds993@gmail.com>
2018-03-17 15:01:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var dragndrop = (function(){
+    
+   var myX = '';
+  var myY = '';
+  var whichArt = '';
+
+  function resetZ()
+    {
+        var elements = document.querySelectorAll('img');
+        for(var i = 0; i < elements.length ; i++)
+            {
+                elements[i].zIndex = 5;
+            }
+    }
+
+  function moveStart(e) {
+    whichArt = e.target;
+    myX = e.offsetX === undefined ? e.layerX : e.offsetX;
+    myY = e.offsetY === undefined ? e.layerY : e.offsetY;
+      resetZ();
+      whichArt.style.zIndex = 10;
+  }
+
+  function moveDragOver(e) {
+    e.preventDefault();
+  }
+
+  function moveDrop(e) {
+    e.preventDefault();
+    whichArt.style.left = e.pageX - myX + 'px';
+    whichArt.style.top = e.pageY - myY + 'px';
+  }
+
+  document.querySelector('body').addEventListener('dragstart', moveStart, false);
+  document.querySelector('body').addEventListener('dragover', moveDragOver, false);
+  document.querySelector('body').addEventListener('drop', moveDrop, false);
+    
+})(); 
